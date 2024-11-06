2d134637b234d22af5d81bd0f0e1825fd9426531
Asaduzzaman04 <asadchy152@gmail.com>
2024-11-07 00:26:58 +0600

    cgpa calculation

--- src/Java_Basic_Assignment/cgpaCalculation.java
+++ src/Java_Basic_Assignment/cgpaCalculation.java
@@ -1,20 +1,18 @@
 package Java_Basic_Assignment;
 
+import java.util.Scanner;
+
 public class cgpaCalculation {
     public static void main(String[] args) {
-        double[] totalMarks = {3.5, 4.0, 3.2, 3.8};
+        double[] totalMarks = {3.0, 4.0, 2.2, 3.8};
         int[] creditHours = {3, 4, 2, 3};
         double totalGradePoints = 0;
         int totalCreditHours = 0;
-
-
         for (int i = 0; i < totalMarks.length; i++) {
-        totalGradePoints = totalMarks[i] * creditHours[1];
-    totalCreditHours = creditHours[i];
+            totalGradePoints += totalMarks[i] * creditHours[i];
+            totalCreditHours += creditHours[i];
         }
-
-double cgpa = totalGradePoints / totalCreditHours;
-        System.out.printf("CGPA: %.2f", cgpa);
-
+        double cgpa = totalGradePoints / totalCreditHours;
+        System.out.printf("CGPA : %.2f", cgpa);
     }
 }
